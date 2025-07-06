bb9a585da22feb0322d1432db386fbe7d674870f
KevinProgramador2 <kevinrezende55@outlook.com>
2025-07-06 17:02:20 -0300

    segundi commit

---
+++ StringsMultiplasLinhas/String3.py
@@ -0,0 +1,24 @@
+nome = "Kevin"
+
+mensagem = f"""
+    Olá meu nome e (nome),
+eu stou aprendendo Python.
+    Essa mensagem tem difenrente recuos.
+"""
+
+
+print (mensagem)
+
+print(
+    """
+    ======== MENU =========
+    
+    1- Depositar
+    2 - Sacar
+    0 - Sair
+    
+    =======================
+    
+    Obrigado por usar nosso sistema!!!
+    """
+)
